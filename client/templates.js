54fdf3322553229f58326ca664e7e14d2b04fac1
Mat Schaffer <mat.schaffer@gmail.com>
2009-10-02 18:12:48 -0400

    Added dynamic template loading.

--- client/templates.js
+++ client/templates.js
@@ -1,11 +1,33 @@
-var Templates = {
-  templates: {},
-  
+var TemplateRegistry = function(templatePath) {
+  this.templatePath = templatePath;
+  this.templates = {};
+};
+
+TemplateRegistry.prototype = {
   register: function(name, fn) {
     this.templates[name] = fn;
   },
-  
-  render: function(name, context) {
-    return this.templates[name](context);
+
+  write: function(name, context, container) {
+    container.innerHTML = this.templates[name](context);
+  },
+
+  parse: function(name, context, container) {
+    alert("Client-side parsing fallback not yet implemented.");
+  },
+
+  render: function(name, context, container) {
+    var that = this;
+
+    if (typeof(template) != "function") {
+      $.getScript(this.templatePath + "/" + name + ".js",
+               function() {
+                 that.write(name, context, container);
+               });
+    } else {
+      this.write(name, context, container);
+    }
   }
-}
+};
+
+var Templates = new TemplateRegistry("../server");
